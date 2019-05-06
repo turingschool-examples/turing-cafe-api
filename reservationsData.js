c57ba4959b5c662f2792e4cd23594c9be993b098
Leta Keane <letakeane+turing@gmail.com>
2019-05-06 17:54:15 -0600

    Add mock data and babel

---
+++ reservationsData.js
@@ -0,0 +1,58 @@
+const reservations = [
+  {
+    name: 'Christie',
+    date: '12/29',
+    time: '7:00',
+    number: 12
+  },
+  {
+    name: 'Leta',
+    date: '4/5',
+    time: '7:00',
+    number: 2
+  },
+  {
+    name: 'Pam',
+    date: '1/21',
+    time: '6:00',
+    number: 4
+  },
+  {
+    name: 'Khalid',
+    date: '5/9',
+    time: '7:30',
+    number: 7
+  },
+  {
+    name: 'Will',
+    date: '5/15',
+    time: '6:30',
+    number: 2
+  },
+  {
+    name: 'Eric',
+    date: '5/30',
+    time: '6:00',
+    number: 8
+  },
+  {
+    name: 'Robbie',
+    date: '6/5',
+    time: '5:30',
+    number: 2
+  },
+  {
+    name: 'Travis',
+    date: '6/8',
+    time: '7:00',
+    number: 12
+  },
+  {
+    name: 'Brittany',
+    date: '9/9',
+    time: '7:30',
+    number: 3
+  }
+]
+
+export default reservations;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { reservations } from './reservationsData'
 
 const app = express();
 
