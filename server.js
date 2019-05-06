0dce6945e178f482833075760ab6527a8a10ff1d
Leta Keane <letakeane+turing@gmail.com>
2019-05-06 17:39:15 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.locals.title = 'Turing Cafe API'
+
+app.use(cors());
+app.use(express.json());
+
+app.set('port', 3001);
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}!`);
+})
