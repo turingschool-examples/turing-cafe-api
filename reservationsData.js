7a4b8efd1636834afc02b616aa34bbeb377b8895
Leta Keane <letakeane+turing@gmail.com>
2019-05-06 18:49:09 -0600

    Add IDs to mockdata, add delete route

--- reservationsData.js
+++ reservationsData.js
@@ -3,56 +3,65 @@ const reservations = [
     name: 'Christie',
     date: '12/29',
     time: '7:00',
-    number: 12
+    number: 12,
+    id: 1
   },
   {
     name: 'Leta',
     date: '4/5',
     time: '7:00',
-    number: 2
+    number: 2,
+    id: 2
   },
   {
     name: 'Pam',
     date: '1/21',
     time: '6:00',
-    number: 4
+    number: 4,
+    id: 3
   },
   {
     name: 'Khalid',
     date: '5/9',
     time: '7:30',
-    number: 7
+    number: 7,
+    id: 4
   },
   {
     name: 'Will',
     date: '5/15',
     time: '6:30',
-    number: 2
+    number: 2,
+    id: 5
   },
   {
     name: 'Eric',
     date: '5/30',
     time: '6:00',
-    number: 8
+    number: 8,
+    id: 6
   },
   {
     name: 'Robbie',
     date: '6/5',
     time: '5:30',
-    number: 2
+    number: 2,
+    id: 7
   },
   {
     name: 'Travis',
     date: '6/8',
     time: '7:00',
-    number: 12
+    number: 12,
+    id: 8
   },
   {
     name: 'Brittany',
     date: '9/9',
     time: '7:30',
-    number: 3
+    number: 3,
+    id: 9
   }
 ]
 
-export default reservations;
+module.exports = reservations;
--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-import express from 'express';
-import cors from 'cors';
-import { reservations } from './reservationsData'
+const express = require('express');
+const cors = require('cors');
+const reservations = require('./reservationsData');
 
 const app = express();
 
 app.locals.title = 'Turing Cafe API'
+app.locals.reservations = reservations;
 
 app.use(cors());
 app.use(express.json());
 
 app.set('port', 3001);
 
+app.get('/api/v1/reservations', (request, response) => {
+  return response.status(200).json(app.locals.reservations)
+});
 
+app.post('/api/v1/reservations', (request, response) => {
+  const newReservation = {...request.body, id: Date.now()};
+
+  for (let requiredParameter of ['name', 'date', 'time', 'number']) {
+    if (!newReservation[requiredParameter]) {
+      return response.status(422).json({
+        error: `Expected format { name: <String>, date: <String>, time: <String>, number: <Number> }. You are missing a required parameter of ${requiredParameter}.`
+      })
+    }
+
+    app.locals.reservations = [...app.locals.reservations, newReservation];
+
+    return response.status(201).json(app.locals.reservations);
+  }
+});
+
+app.delete('/api/v1/reservations/:id', (request, response) => {
+  const { id } = request.params;
+  const parsedId = parseInt(id);
+  const match = app.locals.reservations.find(reservation => parseInt(reservation.id) === parsedId);
+
+  if (!match) {
+    return response.status(404).json({ error: `No reservation found with an id of ${id}.` })
+  }
+
+  const updatedReservations = app.locals.reservations.filter(reservation => parseInt(reservation.id) !== parsedId);
+
+  app.locals.reservations = updatedReservations;
+
+  return response.status(202).json(app.locals.reservations)
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}!`);
